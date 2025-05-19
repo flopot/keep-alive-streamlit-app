cbf0963ab593a90f6588533302a360e85d9ddbd5
flopot <48916418+flopot@users.noreply.github.com>
2025-05-19 14:30:11 +0200

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -9,7 +9,8 @@ const urls = [
   'https://sitemaps-scraper.streamlit.app/',
   'https://bulk-chatgpt.streamlit.app/',
   'https://status-code-checker.streamlit.app/',
-  'https://huggingface.co/spaces/Flopot2/keyword-clusterizer'
+  'https://huggingface.co/spaces/Flopot2/keyword-clusterizer',
+  'https://huggingface.co/spaces/Flopot2/404-redirect-mapper'
 ];
 
 (async () => {
